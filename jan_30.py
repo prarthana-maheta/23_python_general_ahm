99b5e5e8d312929203c341d304a15aa0eced0e14
prarthana.m <prarthana.m@simformsolutions.com>
2024-01-30 20:46:05 +0530

    latest code

---
+++ jan_30.py
@@ -0,0 +1,45 @@
+# how to add elements in list
+
+# list1=[1,2,3]
+# # add 5 to this list
+# list1.append(5)
+# print(list1)
+
+# list1.insert(10,5)
+# print(list1)
+
+# list1.extend(345)
+# print(list1)
+
+
+# list2=['34',78,'67','12']
+# add 45 and '45' to list2
+# add 78 at the last Index
+# add 'royal','pvt','ltd' in the list2
+# add 100 to list2
+# add 45 at fifth index
+
+
+
+# deleting elements from the list
+
+list1=[1,2,3]
+# list1.remove(10)
+# print(list1)
+
+
+# e =list1.pop()
+# list1.insert(0,e)
+# print(list1)
+
+# list1.clear()
+# print(list1)
+
+# del list1[0:2]
+# print(list1)
+
+
+remove 6th element from the list
+remove any element form the list
+clear whole list
+delete all element expect 7,8,9,10
